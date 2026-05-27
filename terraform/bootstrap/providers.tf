01c9091d0a9099a714d1921be220fccfb98fc037
Dawid <dbembnista1@gmail.com>
2026-05-27 21:47:16 +0200

    refactor: migrate OIDC configuration to bootstrap state

--- terraform/bootstrap/providers.tf
+++ terraform/bootstrap/providers.tf
@@ -8,9 +8,22 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "github" {
+  owner = var.github_owner
+  token = var.github_token
 }
